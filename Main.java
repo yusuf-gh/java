7d14fb088bf0263130a26258417808fab760fe9c
Little <146277525+yusuf-gh@users.noreply.github.com>
2025-08-26 14:19:49 +0500

    data types

--- Main.java
+++ Main.java
@@ -1,3 +1,25 @@
 public class Main {
-    
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+
+        byte num = 127;
+        short num1 = 128;
+        int num2 = 56;
+        long num3 = 123456789;
+
+        // byte - от -128 до 127 и занимает 1 байт
+        // short - от -32768 до 32767 и занимает 2 байта
+        // int - от -2147483648 до 2147483647 и занимает 4 байта
+        // long - от -9 223 372 036 854 775 808 до 9 223 372 036 854 775 807 и занимает 8 байт
+
+        float num4 = 4.13413234f;
+        double num5 = 5.12345678676776233253d;
+
+        char ch = '&';
+        String username = "Alex";
+
+        boolean isActive = false;
+
+
+    }
 }
