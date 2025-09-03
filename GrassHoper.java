cc74888ff10643eb0be181b2477cb5d6727d8b37
Little <146277525+yusuf-gh@users.noreply.github.com>
2025-09-03 13:40:36 +0500

    GrassHoper quiz

---
+++ GrassHoper.java
@@ -0,0 +1,16 @@
+public class GrassHoper {
+    public static char getGrade(int s1, int s2, int s3) {
+        int avg = (s1 + s2 + s3) / 3;
+        if (avg >= 90){
+            return 'A';
+        } else if (avg >= 80 && 90 > avg){
+            return 'B';
+        } else if (avg >= 70 && 80 > avg){
+            return 'C';
+        } else if (avg >= 60 && 70 > avg){
+            return 'D';
+        } else {
+            return 'F';
+        }
+    }
+}
