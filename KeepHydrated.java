258fc3af5623f83c3033081daee48cedf41aafd5
Little <146277525+yusuf-gh@users.noreply.github.com>
2025-09-04 17:14:30 +0500

    KeepHydrated quiz

---
+++ KeepHydrated.java
@@ -0,0 +1,6 @@
+public class KeepHydrated {
+    public static int liters(double time)  {    
+        return (int) Math.floor(time * 0.5);
+        //Your code goes here! Hint: You should change that -1
+    }
+}
